999763f7b97ddbdef0ee781299258b8c2229b551
Beach Linnea <blinnea@mi-u6.21-school.ru>
2019-08-19 19:06:40 +0300

    libmap small correction blinnea

--- includes/libmap.h
+++ includes/libmap.h
@@ -6,7 +6,7 @@
 /*   By: blinnea <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/19 17:32:46 by blinnea           #+#    #+#             */
-/*   Updated: 2019/08/19 18:54:22 by lhitmonc         ###   ########.fr       */
+/*   Updated: 2019/08/19 19:06:17 by blinnea          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,11 @@
 # define LIBMAP_H
 
 # include "libft.h"
+
+# define IS_CORRECT(c) (c && c != '\n')
+
+# define OK 0
+# define ERR -1
 
 typedef struct	s_map
 {
@@ -25,10 +30,6 @@ typedef struct	s_map
 	char		filled;
 	char		**body;
 }				t_map;
-
-# define IS_CORRECT(c) (c && c != '\n')
-# define OK 0
-# define ERR 1
 
 int				get_map_height_eof(int stream, t_map *map);
 int				get_map_length(int *stream, t_map *map);
