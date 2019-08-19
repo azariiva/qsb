8cedca7cc289708041c8e2e56bdfd4e38b9e3354
Beach Linnea <blinnea@mi-u6.21-school.ru>
2019-08-19 20:50:38 +0300

    edited prototype and func

--- includes/libmap.h
+++ includes/libmap.h
@@ -6,7 +6,7 @@
 /*   By: blinnea <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/19 17:32:46 by blinnea           #+#    #+#             */
-/*   Updated: 2019/08/19 20:29:04 by fhilary          ###   ########.fr       */
+/*   Updated: 2019/08/19 20:49:27 by blinnea          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,7 +33,7 @@ int				is_correct_sym(char c);
 int				get_map_height_eof(int stream, t_map *map);
 int				get_map_length(int *stream, t_map *map, char *filename);
 int				get_map_body(int stream, t_map *map);
-t_map			*get_map(int stream);
+t_map			*get_map(int stream, char *filename);
 void			print_map(int stream, t_map *map);
 
 #endif
--- srcs/libmap/get_map.c
+++ srcs/libmap/get_map.c
@@ -6,13 +6,13 @@
 /*   By: fhilary <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/19 19:47:51 by fhilary           #+#    #+#             */
-/*   Updated: 2019/08/19 20:02:52 by fhilary          ###   ########.fr       */
+/*   Updated: 2019/08/19 20:49:33 by blinnea          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libmap.h"
 
-t_map	*get_map(int stream)
+t_map	*get_map(int stream, char *filename)
 {
 	t_map	*map;
 	int		i;
@@ -20,7 +20,7 @@ t_map	*get_map(int stream)
 	map = NULL;
 	i = 0;
 	if (!(get_map_height_eof(stream, map) && \
-				get_map_length(stream, map)))
+				get_map_length(stream, map, filename)))
 		return (NULL);
 	map->body = (t_map**)malloc(sizeof(t_map*) * map->height);
 	while (i < map->height)
