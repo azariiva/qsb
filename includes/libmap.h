6b9f8725145cca7a61792210f48502333c57c403
Beach Linnea <blinnea@mi-u6.21-school.ru>
2019-08-20 15:20:54 +0300

    deleted old functions edited proto

--- includes/libmap.h
+++ includes/libmap.h
@@ -6,7 +6,7 @@
 /*   By: blinnea <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/19 17:32:46 by blinnea           #+#    #+#             */
-/*   Updated: 2019/08/20 12:51:19 by blinnea          ###   ########.fr       */
+/*   Updated: 2019/08/20 15:19:00 by blinnea          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,15 +27,12 @@ typedef struct	s_map
 	char		empty;
 	char		obstacle;
 	char		filled;
-	char		**body;
+	int			*prev_line;
+	int			*cur_line;
 }				t_map;
 
 int				is_correct_sym(char c);
 int				get_map_height_eof(int stream, t_map *map);
 int				get_map_length(int *stream, t_map *map, char *filename);
-int				get_map_body(int stream, t_map *map);
-t_map			*get_map(int *stream, char *filename);
-void			print_map(int stream, t_map *map);
-void			free_map(t_map *map, size_t real_height);
 
 #endif
