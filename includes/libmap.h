e2a78a0d97c24b11b91a2ddefe0e2ecf3ef14bc9
Beach Linnea <blinnea@mi-u6.21-school.ru>
2019-08-20 12:51:47 +0300

    free_map fixed

--- includes/libmap.h
+++ includes/libmap.h
@@ -6,7 +6,7 @@
 /*   By: blinnea <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/19 17:32:46 by blinnea           #+#    #+#             */
-/*   Updated: 2019/08/19 21:24:44 by blinnea          ###   ########.fr       */
+/*   Updated: 2019/08/20 12:51:19 by blinnea          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,5 +36,6 @@ int				get_map_length(int *stream, t_map *map, char *filename);
 int				get_map_body(int stream, t_map *map);
 t_map			*get_map(int *stream, char *filename);
 void			print_map(int stream, t_map *map);
+void			free_map(t_map *map, size_t real_height);
 
 #endif
--- srcs/libmap/free_map.c
+++ srcs/libmap/free_map.c
@@ -6,14 +6,19 @@
 /*   By: rmaple <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/19 22:15:21 by rmaple            #+#    #+#             */
-/*   Updated: 2019/08/19 23:19:04 by rmaple           ###   ########.fr       */
+/*   Updated: 2019/08/20 12:50:49 by blinnea          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libmap.h"
 
-void	free_map(t_map *map)
+void	free_map(t_map *map, size_t real_height)
 {
-		free(map->body);
-		free(map);
+	size_t	i;
+
+	i = 0;
+	while (i < real_height)
+		free(map->body[i]);
+	free(map->body);
+	free(map);
 }
